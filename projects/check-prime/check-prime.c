07da6c1a475f3c8da0a564d53e63c0130fd42da6
Yaksh Bariya <yakshbari4@gmail.com>
2021-03-15 20:49:14 +0530

    Format code using clang-format

--- projects/check-prime/check-prime.c
+++ projects/check-prime/check-prime.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int check_prime(int input) {
-	       if(input <= 1)
-			                return 0;                               int factor_count = 0;
-		        for(int check = 1; check <= input; check++){                                                            if(input%check == 0)
-				                        factor_count++;
-							        }
-			        return factor_count==2;
+  if (input <= 1)
+    return 0;
+  int factor_count = 0;
+  for (int check = 1; check <= input; check++) {
+    if (input % check == 0)
+      factor_count++;
+  }
+  return factor_count == 2;
 }
 
 int main() {
-	printf("Enter number: ");
-	int input;
-	scanf("%d", &input);
-	printf("Input number %d was %sprime", input, check_prime(input) ? "" : "not ");
+  printf("Enter number: ");
+  int input;
+  scanf("%d", &input);
+  printf("Input number %d was %sprime", input,
+         check_prime(input) ? "" : "not ");
 }
